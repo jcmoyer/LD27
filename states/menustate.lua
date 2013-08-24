e2707efc0a4b02379e451bcda3417b34d3cdf9b4
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-23 23:52:37 -0400

    menustate constructor

--- states/menustate.lua
+++ states/menustate.lua
@@ -1,5 +1,11 @@
 local gamestate = require('core.gamestate')
+local playstate = require('states.playstate')
 
 local menustate = setmetatable({}, {__index = gamestate})
+local mt = {__index = menustate}
+
+function menustate.new()
+  return setmetatable({}, mt)
+end
 
 return menustate
