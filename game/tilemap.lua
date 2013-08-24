51ca4d66131303919982331ffa4309fccd7243ae
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-24 03:25:39 -0400

    Tilemap object

---
+++ game/tilemap.lua
@@ -0,0 +1,17 @@
+local tilemap = {}
+local mt = {__index = tilemap}
+
+function tilemap.new(data, width, height)
+  local instance = {
+    data = data,
+    width = width,
+    height = height
+  }
+  return setmetatable(instance, mt)
+end
+
+function tilemap:at(x, y)
+  return self.data[(y - 1) * self.width + x]
+end
+
+return tilemap
