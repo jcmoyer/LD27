a0277e325d654f013f1065da695dca4a263d75ff
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-23 23:51:51 -0400

    playstate constructor

--- states/playstate.lua
+++ states/playstate.lua
@@ -1,5 +1,10 @@
 local gamestate = require('core.gamestate')
 
 local playstate = setmetatable({}, {__index = gamestate})
+local mt = {__index = playstate}
+
+function playstate.new()
+  return setmetatable({}, mt)
+end
 
 return playstate
