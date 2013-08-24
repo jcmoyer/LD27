b9db8ba9485566e3957c270cef1e7817629c8e4f
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-24 18:19:08 -0400

    Actor object

---
+++ game/actor.lua
@@ -0,0 +1,67 @@
+local rectangle = require('game.rectangle')
+
+local actor = {}
+local mt = {__index = actor}
+
+function actor.new(x, y)
+  local instance = {
+    x = x,
+    y = y,
+    w = 32,
+    h = 64,
+    vx = 0,
+    vy = 0,
+    hb = rectangle.new(x or 0, y or 0, 32, 64)
+  }
+  return setmetatable(instance, mt)
+end
+
+function actor:hitbox()
+  local hitbox = self.hb
+  hitbox.x = self.x
+  hitbox.y = self.y
+  return hitbox
+end
+
+-- takes the level to resolve collision within
+function actor:update(level)
+  self.x = self.x + self.vx
+  local hitbox = self:hitbox()
+  
+  -- X axis
+  -- we have to fudge the numbers a bit; if we don't subtract 1 from the bottom weird things happen
+  for y = hitbox.y, hitbox:bottom() - 1 do
+    local solid, tx = level:solidAt(hitbox.x, y)
+    if solid then
+      self.x = tx
+      self.vx = 0
+    end
+    
+    solid, tx = level:solidAt(hitbox:right(), y)
+    if solid then
+      self.x = tx - self.w - level.tilewidth
+      self.vx = 0
+    end
+  end
+  
+  self.y = self.y + self.vy
+  hitbox = self:hitbox()
+  
+  -- Y axis
+  -- same as above
+  for x = hitbox.x, hitbox:right() - 1 do
+    local solid, _, ty = level:solidAt(x, hitbox:bottom())
+    if solid then
+      self.y = ty - self.h
+      self.vy = 0
+    end
+    
+    solid, _, ty = level:solidAt(x, hitbox.y)
+    if solid then
+      self.y = ty + level.tileheight
+      self.vy = 0
+    end
+  end
+end
+
+return actor
