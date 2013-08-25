c235619b2f3cf39e598828aa938fe9ae98dc4d0b
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-25 14:35:23 -0400

    Make projectiles expire and silent if out of range

--- data/controllers/projectiles.lua
+++ data/controllers/projectiles.lua
@@ -1,8 +1,15 @@
+local time = 0
+local maxtime = 2
+
 local function onTick(context, dt)
-  if context.atWall() then
-    context.playSound('hit')
+  if context.atWall() or time >= maxtime then
+    if context.playerDistance() < 500 then
+      context.playSound('hit')
+    end
     context.kill()
   end
+  
+  time = time + dt
 end
 
 local function onCollide(context, actor)
