d40c15c2ca642f8f7e8067b3da1cf8447f854bb4
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-24 20:52:54 -0400

    Expiring lives mechanic

--- states/playstate.lua
+++ states/playstate.lua
@@ -4,9 +4,19 @@ local player = require('game.player')
 local camera = require('core.camera')
 local controller = require('game.controller')
 local mathex = require('core.extensions.math')
+local fontpool = require('core.fontpool')
 
 local playstate = setmetatable({}, {__index = gamestate})
 local mt = {__index = playstate}
+
+local lifeFont = fontpool.get(18)
+local lifeHudH = lifeFont:getHeight()
+
+local function lifetimeStr(x)
+  local sec  = x
+  local msec = 1000 * (x - math.floor(x))
+  return string.format('%02d:%03d', sec, msec)
+end
 
 function playstate:changelevel(name)
   self.level  = level.new(name)
@@ -21,6 +31,9 @@ function playstate.new()
     level      = nil,
     player     = nil,
     controller = nil,
+    
+    lives      = 3,
+    lifetime   = 10,
     
     camera = camera.new(love.graphics.getWidth(), love.graphics.getHeight())
   }
@@ -55,14 +68,33 @@ function playstate:update(dt)
   -- Lock camera to level boundaries
   self.camera.x = mathex.clamp(self.camera.x, -self.level.width  * self.level.tilewidth  + love.graphics.getWidth(), 0)
   self.camera.y = mathex.clamp(self.camera.y, -self.level.height * self.level.tileheight + love.graphics.getHeight(), 0)
+  
+  self.lifetime = self.lifetime - dt
+  
+  if self.lifetime <= 0 then
+    self.lives    = self.lives - 1
+    self.lifetime = 10
+  end
 end
 
 function playstate:draw()
+  love.graphics.push()
+  
   love.graphics.translate(math.floor(self.camera.x), math.floor(self.camera.y))
   self.level:draw(self.camera)
   
   -- draw player
   love.graphics.rectangle('fill', self.player:hitbox():unpack())
+  
+  love.graphics.pop()
+  
+  love.graphics.setColor(0, 0, 0, 128)
+  love.graphics.rectangle('fill', love.graphics.getWidth() - 145, 0, 145, lifeHudH)
+  
+  love.graphics.setColor(255, 255, 255, 255)
+  love.graphics.setFont(lifeFont)
+  love.graphics.print(string.format('%05d', self.lives), love.graphics.getWidth() - 140, 0)
+  love.graphics.print(lifetimeStr(self.lifetime), love.graphics.getWidth() - 65, 0)
 end
 
 return playstate
