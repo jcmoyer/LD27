325a8ea0681e738316d3e3195adb9359003cf79a
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-24 21:15:17 -0400

    Portal object

---
+++ game/portal.lua
@@ -0,0 +1,23 @@
+local rectangle = require('game.rectangle')
+
+local portal = {}
+local mt = {__index = portal}
+
+function portal.new(x, y, destination)
+  local instance = {
+    x = x,
+    y = y,
+    destination = destination,
+    hb = rectangle.new(x, y, 64, 96)
+  }
+  return setmetatable(instance, mt)
+end
+
+function portal:hitbox()
+  local hitbox = self.hb
+  hitbox.x = self.x
+  hitbox.y = self.y
+  return hitbox
+end
+
+return portal
