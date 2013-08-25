0aa204371dec770409539fff231e459b0d547e95
J.C. Moyer <jmoyer1992@gmail.com>
2013-08-25 09:10:36 -0400

    Sound management module

---
+++ game/sounds.lua
@@ -0,0 +1,20 @@
+local sounds = {}
+
+local soundfiles = love.filesystem.enumerate('data/sounds')
+
+for i = 1, #soundfiles do
+  local _, _, name = soundfiles[i]:find('([%a%d_]+)%.ogg$')
+  if name then
+    sounds[name] = love.audio.newSource('data/sounds/' .. name .. '.ogg', 'static')
+  end
+end
+
+function sounds.play(name)
+  local s = sounds[name]
+  if s ~= nil then
+    s:stop()
+    s:play()
+  end
+end
+
+return sounds
