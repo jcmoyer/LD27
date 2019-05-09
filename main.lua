1ff9d71ca5da06932efd251cfb3f6c48ba0c66b0
J.C. Moyer <jcmoyer32@gmail.com>
2019-05-09 00:24:23 -0400

    Update for love 11.2

--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
 -- Look for modules in lib/ directory
 package.path = "./lib/?.lua;" .. package.path
 
+local setColor = love.graphics.setColor
+love.graphics.setColor = function(r, g, b, a)
+  if type(r) == 'table' then
+    love.graphics.setColor(unpack(r))
+  else
+    setColor(r / 255, g / 255, b / 255, (a or 255) / 255)
+  end
+end
+
 local basicgame = require('hug.basicgame')
 local gameloop  = require('hug.gameloop')
 local menustate = require('states.menustate')
